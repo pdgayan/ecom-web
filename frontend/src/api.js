62384a0d11f5ba434dd393790a209fe6d4129473
Gayan-98 <dongayanpathirage@gmail.com>
2026-07-29 23:50:32 +0530

    configure ingress path

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,5 +1,7 @@
-export const AUTH_URL     = 'http://localhost:4001';
-export const CATALOG_URL  = 'http://localhost:4002';
-export const CART_URL     = 'http://localhost:4003';
-export const ORDER_URL    = 'http://localhost:4004';
-export const PAYMENT_URL  = 'http://localhost:4005';
+const API_BASE = import.meta.env.VITE_API_URL;
+
+export const AUTH_URL = `${API_BASE}/auth`;
+export const CATALOG_URL = `${API_BASE}/catalog`;
+export const CART_URL = `${API_BASE}/cart`;
+export const ORDER_URL = `${API_BASE}/orders`;
+export const PAYMENT_URL = `${API_BASE}/payment`;
